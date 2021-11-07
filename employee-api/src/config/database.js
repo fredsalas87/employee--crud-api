e76bc896278b5c81c725850cfaaa99ec77ded003
fredsalas87 <fredsalas87@gmail.com>
2021-11-07 16:34:25 -0300

    Desenvolvimento do arquivo 'database.js'

--- employee-api/src/config/database.js
+++ employee-api/src/config/database.js
@@ -4,3 +4,25 @@
  * data: 06/11/2021
  */
 
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// ==> Conexãop con a Base de Dados:
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+pool.on('error', (err) => {
+  console.log('Unexpected error on idle client', err)
+  process.exit(-1);
+});
+
+pool.on('connect', () => {
+  console.log('Base de Dados conectado com sucesso!')
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+}
